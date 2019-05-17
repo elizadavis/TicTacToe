3ee113a156271a4be8be9c37f445f833f60830ce
elizadavis <eelizadavis@gmail.com>
2019-05-17 18:32:58 -0400

    Add gameplay method
    Moved gameplay code into onBoxClick function with PATCH request

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -1,13 +1,16 @@
 'use strict'
-const getFormFields = require('../../../lib/get-form-fields')
+// const getFormFields = require('../../../lib/get-form-fields')
 const api = require('./api')
 const ui = require('./ui')
+const store = require('../store')
 
-const onCreate = event => {
+store.currentPlayer = 'x'
+
+const onCreate = () => {
   event.preventDefault()
-  const form = event.target
-  const formData = getFormFields(form)
-  api.create(formData)
+  $('.box').text('')
+  $('#game-message').text('')
+  api.create()
     .then(ui.onCreateSuccess)
     .catch(ui.onCreateFailure)
 }
@@ -18,17 +21,78 @@ const onIndex = event => {
     .then(ui.onIndexSuccess)
     .catch(ui.onIndexFailure)
 }
-const onUpdate = event => {
+
+const checkForXWin = function () {
+  const gArray = store.game.cells
+  if (gArray[0] && gArray[0] === gArray[1] && gArray[0] === gArray[2] && gArray[2] === 'x') {
+    return true
+  } else if (gArray[0] && gArray[0] === gArray[4] && gArray[0] === gArray[8] && gArray[8] === 'x') {
+    return true
+  } else if (gArray[0] && gArray[0] === gArray[3] && gArray[0] === gArray[6] && gArray[6] === 'x') {
+    return true
+  } else if (gArray[1] && gArray[1] === gArray[4] && gArray[1] === gArray[7] && gArray[7] === 'x') {
+    return true
+  } else if (gArray[2] && gArray[2] === gArray[5] && gArray[2] === gArray[8] && gArray[8] === 'x') {
+    return true
+  } else if (gArray[2] && gArray[2] === gArray[4] && gArray[2] === gArray[6] && gArray[6] === 'x') {
+    return true
+  } else if (gArray[3] && gArray[3] === gArray[4] && gArray[3] === gArray[5] && gArray[5] === 'x') {
+    return true
+  } else if (gArray[6] && gArray[6] === gArray[7] && gArray[6] === gArray[8] && gArray[8] === 'x') {
+    return true
+  }
+}
+
+const checkForOWin = function () {
+  const gArray = store.game.cells
+  if (gArray[0] && gArray[0] === gArray[1] && gArray[0] === gArray[2] && gArray[2] === 'o') {
+    return true
+  } else if (gArray[0] && gArray[0] === gArray[4] && gArray[0] === gArray[8] && gArray[8] === 'o') {
+    return true
+  } else if (gArray[0] && gArray[0] === gArray[3] && gArray[0] === gArray[6] && gArray[6] === 'o') {
+    return true
+  } else if (gArray[1] && gArray[1] === gArray[4] && gArray[1] === gArray[7] && gArray[7] === 'o') {
+    return true
+  } else if (gArray[2] && gArray[2] === gArray[5] && gArray[2] === gArray[8] && gArray[8] === 'o') {
+    return true
+  } else if (gArray[2] && gArray[2] === gArray[4] && gArray[2] === gArray[6] && gArray[6] === 'o') {
+    return true
+  } else if (gArray[3] && gArray[3] === gArray[4] && gArray[3] === gArray[5] && gArray[5] === 'o') {
+    return true
+  } else if (gArray[6] && gArray[6] === gArray[7] && gArray[6] === gArray[8] && gArray[8] === 'o') {
+    return true
+  }
+}
+
+const onBoxClick = event => {
   event.preventDefault()
-  const form = event.target
-  const formData = getFormFields(form)
-  api.update(formData)
-    .then(ui.onUpdateSuccess)
-    .catch(ui.onUpdateFailure)
+  const cell = ($(event.target).data('cell-index'))
+  if (!store.game.cells[cell]) {
+    store.game.cells[cell] = store.currentPlayer
+    $(event.target).text(`${store.currentPlayer}`)
+    if (checkForXWin() === true) {
+      $('#game-message').text('x wins!')
+      store.game.over = true
+    }
+    if (checkForOWin() === true) {
+      $('#game-message').text('o wins!')
+      store.game.over = true
+    }
+    api.update(cell)
+      .then(ui.onUpdateSuccess)
+      .then(store.currentPlayer = store.currentPlayer === 'x' ? 'o' : 'x')
+      .catch(ui.onUpdateFailure)
+  } else {
+    $('#game-message').text('space already marked')
+  }
+  // if (checkForOWin() === false && checkForXWin() === false) {
+  //   $('#game-message').text('Draw - Game Over')
+  //   store.game.over = true
+  // }
 }
 
 module.exports = {
   onCreate,
   onIndex,
-  onUpdate
+  onBoxClick
 }
