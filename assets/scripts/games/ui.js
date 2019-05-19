c6dd7f670f26a84e5d72698456c17eae30aba915
elizadavis <eelizadavis@gmail.com>
2019-05-19 18:44:37 -0400

    View past game
    Added ability to view moves from finished games on board

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -19,7 +19,10 @@ const onCreateFailure = responseData => {
 
 const onIndexSuccess = responseData => {
   console.log('success', responseData)
-  // const games = responseData.games
+  const games = responseData.games
+  const complete = games.filter(game => game.over)
+  $('#get-all-games').text(`You have started ${games.length} games and finished ${complete.length} of those games.`)
+  $('#game-board').removeClass('hidden')
   $('#message').removeClass()
   $('#message').addClass('success')
 }
@@ -43,11 +46,38 @@ const onUpdateFailure = responseData => {
   $('#message').addClass('failure')
 }
 
+const onShowSuccess = responseData => {
+  console.log('success', responseData)
+  const gameId = responseData.game.id
+  const moves = responseData.game.cells
+  $('#0').text(`${moves[0]}`)
+  $('#1').text(`${moves[1]}`)
+  $('#2').text(`${moves[2]}`)
+  $('#3').text(`${moves[3]}`)
+  $('#4').text(`${moves[4]}`)
+  $('#5').text(`${moves[5]}`)
+  $('#6').text(`${moves[6]}`)
+  $('#7').text(`${moves[7]}`)
+  $('#8').text(`${moves[8]}`)
+  $('#show-game').text('Showing example: ' + gameId)
+  $('#game-board').removeClass('hidden')
+  $('#message').addClass('success')
+}
+
+const onShowFailure = responseData => {
+  console.log('failure', responseData)
+  $('#message').text('Failed to get :(')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+}
+
 module.exports = {
   onCreateSuccess,
   onCreateFailure,
   onIndexSuccess,
   onIndexFailure,
   onUpdateSuccess,
-  onUpdateFailure
+  onUpdateFailure,
+  onShowSuccess,
+  onShowFailure
 }
