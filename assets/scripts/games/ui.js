26d6b0a2743b0120f32181f72811f7388071c030
elizadavis <eelizadavis@gmail.com>
2019-05-20 10:42:32 -0400

    Fix box IDs
    Removed IDs from boxes in html and updated to use data cell index

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -18,55 +18,51 @@ const onCreateFailure = responseData => {
 }
 
 const onIndexSuccess = responseData => {
-  console.log('success', responseData)
   const games = responseData.games
   const complete = games.filter(game => game.over)
   $('#get-all-games').text(`You have started ${games.length} games and finished ${complete.length} of those games.`)
-  $('#game-board').removeClass('hidden')
   $('#message').removeClass()
   $('#message').addClass('success')
 }
 
 const onIndexFailure = responseData => {
-  console.log('failure', responseData)
-  $('#message').text('Failed to get all games')
+  $('#get-all-games').text('Failed to get game stats')
   $('#message').removeClass()
   $('#message').addClass('failure')
 }
 
 const onUpdateSuccess = responseData => {
   store.game = responseData.game
-  console.log('success', responseData)
+  if (!store.game.over) {
+    $('#game-message').text(`Your turn, ${store.currentPlayer}!`)
+  }
 }
 
 const onUpdateFailure = responseData => {
-  console.log('failure', responseData)
   $('#message').text('Failed to update')
   $('#message').removeClass()
   $('#message').addClass('failure')
 }
 
 const onShowSuccess = responseData => {
-  console.log('success', responseData)
   const gameId = responseData.game.id
   const moves = responseData.game.cells
-  $('#0').text(`${moves[0]}`)
-  $('#1').text(`${moves[1]}`)
-  $('#2').text(`${moves[2]}`)
-  $('#3').text(`${moves[3]}`)
-  $('#4').text(`${moves[4]}`)
-  $('#5').text(`${moves[5]}`)
-  $('#6').text(`${moves[6]}`)
-  $('#7').text(`${moves[7]}`)
-  $('#8').text(`${moves[8]}`)
-  $('#show-game').text('Showing example: ' + gameId)
+  $('[data-cell-index=0]').text(`${moves[0]}`)
+  $('[data-cell-index=1]').text(`${moves[1]}`)
+  $('[data-cell-index=2]').text(`${moves[2]}`)
+  $('[data-cell-index=3]').text(`${moves[3]}`)
+  $('[data-cell-index=4]').text(`${moves[4]}`)
+  $('[data-cell-index=5]').text(`${moves[5]}`)
+  $('[data-cell-index=6]').text(`${moves[6]}`)
+  $('[data-cell-index=7]').text(`${moves[7]}`)
+  $('[data-cell-index=8]').text(`${moves[8]}`)
+  $('#game-message').text('Showing game: ' + gameId)
   $('#game-board').removeClass('hidden')
   $('#message').addClass('success')
 }
 
 const onShowFailure = responseData => {
-  console.log('failure', responseData)
-  $('#message').text('Failed to get :(')
+  $('#game-message').text('Failed to show game')
   $('#message').removeClass()
   $('#message').addClass('failure')
 }
