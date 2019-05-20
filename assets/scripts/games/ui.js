0608fa0052e0c1e1e34a518017d296b9dd899e12
elizadavis <eelizadavis@gmail.com>
2019-05-20 13:45:27 -0400

    Clear field - show
    Added step to clear field after submitting game to show

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -3,7 +3,6 @@ const store = require('../store')
 
 const onCreateSuccess = responseData => {
   store.game = responseData.game
-  console.log('success', responseData)
   $('#message').text('New Game')
   $('#message').removeClass()
   $('#game-board').removeClass('hidden')
@@ -11,7 +10,6 @@ const onCreateSuccess = responseData => {
 }
 
 const onCreateFailure = responseData => {
-  console.log('failure', responseData)
   $('#message').text('Failed to begin new game')
   $('#message').removeClass()
   $('#message').addClass('failure')
@@ -20,7 +18,7 @@ const onCreateFailure = responseData => {
 const onIndexSuccess = responseData => {
   const games = responseData.games
   const complete = games.filter(game => game.over)
-  $('#get-all-games').text(`You have started ${games.length} games and finished ${complete.length} of those games.`)
+  $('#get-all-games').text(`You have played ${complete.length} full games.`)
   $('#message').removeClass()
   $('#message').addClass('success')
 }
@@ -47,16 +45,17 @@ const onUpdateFailure = responseData => {
 const onShowSuccess = responseData => {
   const gameId = responseData.game.id
   const moves = responseData.game.cells
-  $('[data-cell-index=0]').text(`${moves[0]}`)
-  $('[data-cell-index=1]').text(`${moves[1]}`)
-  $('[data-cell-index=2]').text(`${moves[2]}`)
-  $('[data-cell-index=3]').text(`${moves[3]}`)
-  $('[data-cell-index=4]').text(`${moves[4]}`)
-  $('[data-cell-index=5]').text(`${moves[5]}`)
-  $('[data-cell-index=6]').text(`${moves[6]}`)
-  $('[data-cell-index=7]').text(`${moves[7]}`)
-  $('[data-cell-index=8]').text(`${moves[8]}`)
+  $('[data-cell-index=0]').html(`${moves[0]}`)
+  $('[data-cell-index=1]').html(`${moves[1]}`)
+  $('[data-cell-index=2]').html(`${moves[2]}`)
+  $('[data-cell-index=3]').html(`${moves[3]}`)
+  $('[data-cell-index=4]').html(`${moves[4]}`)
+  $('[data-cell-index=5]').html(`${moves[5]}`)
+  $('[data-cell-index=6]').html(`${moves[6]}`)
+  $('[data-cell-index=7]').html(`${moves[7]}`)
+  $('[data-cell-index=8]').html(`${moves[8]}`)
   $('#game-message').text('Showing game: ' + gameId)
+  $('#get-game input[type="number"]').val('')
   $('#game-board').removeClass('hidden')
   $('#message').addClass('success')
 }
