db432e92720874177c79a932bd1e5b2f00e23536
elizadavis <eelizadavis@gmail.com>
2019-05-20 17:57:41 -0400

    Remove class changes
    Removed updates for 'failure' and 'success' classes - not styling these

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,22 +4,16 @@ const store = require('../store')
 
 const onSignUpSuccess = responseData => {
   $('#message').text('Signed up successfully!')
-  $('#message').removeClass()
-  $('#message').addClass('success')
   $('#sign-up input[type="email"]').val('')
   $('#sign-up input[type="password"]').val('')
 }
 
 const onSignUpFailure = responseData => {
   $('#message').text('Sign up failed')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
 }
 
 const onSignInSuccess = responseData => {
   $('#message').text('Signed in successfully!')
-  $('#message').removeClass()
-  $('#message').addClass('success')
   $('#signup-in').hide()
   $('#loggedin').removeClass('hidden')
   $('#game-options').removeClass('hidden')
@@ -28,26 +22,18 @@ const onSignInSuccess = responseData => {
 
 const onSignInFailure = responseData => {
   $('#message').text('Sign in failed')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
 }
 
 const onChangePasswordSuccess = () => {
   $('#message').text('Password changed successfully!')
-  $('#message').removeClass()
-  $('#message').addClass('success')
 }
 
 const onChangePasswordFailure = () => {
   $('#message').text('Unable to change password')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
 }
 
 const onSignOutSuccess = () => {
   $('#message').text('Signed out successfully!')
-  $('#message').removeClass()
-  $('#message').addClass('success')
   $('#signup-in').show()
   $('#game-board').addClass('hidden')
   $('#loggedin').addClass('hidden')
@@ -56,8 +42,6 @@ const onSignOutSuccess = () => {
 
 const onSignOutFailure = () => {
   $('#message').text('Sign out failed')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
 }
 
 module.exports = {
